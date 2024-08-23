025abfa97854f51c67d3cd91d3d9315e6437100d
Emin Yagiz <eminyagiz42@gmail.com>
2024-08-23 16:43:44 +0300

    hackerrank day0,day1 javascript

---
+++ hackerrank/Day0.js
@@ -0,0 +1,21 @@
+/* hello-world */
+
+const parameterVariable = 'Welcome to 10 Days of JavaScript!';
+
+console.log('Hello, World!');
+console.log(parameterVariable);
+
+
+/* data-types */
+
+const secondInteger  = 12;
+const secondDecimal  = 4.32;
+const secondString = 'is the best place to learn and practice coding!';
+
+const firstInteger = 4;
+const firstDecimal = 4.0;
+const firstString = 'HackerRank';
+
+console.log(Number(firstInteger) + Number(secondInteger));
+console.log(Number(firstDecimal) + Number(secondDecimal));
+console.log(firstString + secondString);
--- hackerrank/Day1.js
+++ hackerrank/Day1.js
@@ -1,1 +1,37 @@
-console.log("Hello, World!");
+/* arithmetic-operators */
+
+const length = 3;
+const width =  4.5;
+
+console.log(getArea(length, width));
+console.log(getPerimeter(length, width));
+
+
+
+function getArea(length, width) {
+    return Number(length) * Number(width);
+}
+
+
+function getPerimeter(length, width) {
+    return  (Number(length) + Number(width)) *  2;
+}
+
+/* function */
+
+console.log(factorial(4))
+
+function factorial(n) {
+    if(n === 1){
+        return 1;
+    }
+    return factorial(n-1) * n;
+}
+
+
+/* let-and-const */
+
+let r = readLine();
+const pi = Math.PI;
+console.log(pi * Math.pow (r,2));
+console.log(2 * pi * r);
