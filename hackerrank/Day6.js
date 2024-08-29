4e69f356cf508231c69c6c290c6b3d5cb8def497
Emin Yagiz <eminyagiz42@gmail.com>
2024-08-29 18:16:32 +0300

    hackerrank day6 javascript

---
+++ hackerrank/Day6.js
@@ -0,0 +1,33 @@
+/* 10 Days of Javascript - bitwise */
+
+function getMaxLessThanK(n, k) {
+    let max = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j <= n; j++) {
+            let and = i & j;
+            if (and < k && and > max){
+                max = and;
+
+            }
+        }
+    }
+    return max;
+}
+
+const  n = 9;
+const  k = 2;
+
+console.log(getMaxLessThanK(n, k));
+console.log( ' ')
+
+
+
+/* 10 Days of Javascript - date */
+
+function getDayName(dateString) {
+    return new Date(dateString).toLocaleDateString('en-US', { weekday: 'long' }); //Sunday
+}
+
+const date = "10/11/2009";
+
+console.log(getDayName(date));
