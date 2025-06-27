0f753ef9e0c78f827c5267e5b8fb0664f196168f
Emin Yagiz <eminyagiz42@gmail.com>
2025-06-27 17:07:50 +1200

    Array forEach taking Notes 3 from W3Schools JS

--- w3schools/Notes3.js
+++ w3schools/Notes3.js
@@ -3,3 +3,12 @@ const age = 31;
 let canVote = (age < 18) ? "Too young" : "Old enough";
 
 console.log(canVote); // Old enough
+
+
+const numbers = [45, 4, 9, 16, 25];
+let txt = "";
+numbers.forEach(myFunction);
+
+function myFunction(value, index, array) {
+    txt += value + "<br>";
+}
